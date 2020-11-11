cf3a97ca4e7879a48e669e4b4fd230ff842e34db
Terry L <neomisino@gmail.com>
2020-11-11 11:48:50 -0600

    Changed __resource > fxmanist

---
+++ windowscript/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Roll down Windows Scripts Created By Terry L.
+fx_version 'adamant'
+game 'gta5'
+
+author 'Terry L.'
+description 'Window script'
+version '0.1.0'
+
+-- Script loading...
+client_script 'client.lua'
+server_script 'server.lua'
